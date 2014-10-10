b50a684fe879706582d443bec3cfb97670524727
Stephan Rave <stephanrave@uni-muenster.de>
2014-10-10 16:06:47 +0200

    Create interfaces.h

---
+++ interfaces.h
@@ -0,0 +1,6 @@
+typedef void* object_id;
+
+bool get_stationary_discretization(object_id *d);  // To be replaced later on.
+
+bool get_operator(object_id *d, object_id *op);
+bool get_rhs(object_id *d, object_id *rhs);
